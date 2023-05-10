5971c872803b097eaf264b3674dc5d08c6293f19
tejas-1111 <1f4e7.tejas.chaudhari@gmail.com>
2023-05-10 22:38:41 +0530

    Completed Deep Q

--- dqn.py
+++ dqn.py
@@ -1,4 +1,3 @@
-
 # %%
 import torch
 import torch.nn as nn
@@ -16,7 +15,7 @@ from copy import deepcopy
 number = 2
 NAME = "DQN_RL" + str(number)
 ID = "DQN_RL" + str(number)
-run = wandb.init(project='DQN_RL', name = NAME, id = ID)
+run = wandb.init(project="DQN_RL", name=NAME, id=ID)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -29,12 +28,12 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 # %%
-Transition = namedtuple('Transition',
-                        ('state', 'action', 'reward', 'next_state', 'next_action'))
+Transition = namedtuple(
+    "Transition", ("state", "action", "reward", "next_state", "next_action")
+)
 
 
 class ReplayMemory(object):
-
     def __init__(self, capacity):
         self.memory = deque([], maxlen=capacity)
 
@@ -51,7 +50,6 @@ class ReplayMemory(object):
 
 # %%
 class DQN(nn.Module):
-
     def __init__(self, n_observations, n_actions):
         super(DQN, self).__init__()
         self.layer1 = nn.Linear(n_observations, 16)
@@ -71,7 +69,7 @@ env = Env(R=35)
 
 # %%
 BATCH_SIZE = 512
-GAMMA = 0.7 # 1 # 0.99
+GAMMA = 0.7  # 1 # 0.99
 EPS_START = 0.9
 EPS_END = 0.05
 EPS_DECAY = 1000
@@ -97,19 +95,23 @@ steps_done = 0
 def select_action(state):
     global steps_done
     sample = random.random()
-    eps_threshold = EPS_END + (EPS_START - EPS_END) *         np.exp(-1. * steps_done / EPS_DECAY)
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * np.exp(
+        -1.0 * steps_done / EPS_DECAY
+    )
     steps_done += 1
     if sample > eps_threshold:
         with torch.no_grad():
             return policy_net(state).max(1)[1].view(1, 1)
     else:
-        return torch.tensor([[np.random.randint(0, 2)]], device=device, dtype=torch.long)
+        return torch.tensor(
+            [[np.random.randint(0, 2)]], device=device, dtype=torch.long
+        )
 
 
 # %%
 def optimize_model(timestep=0, batch_num=0, reward=0):
     if len(memory) < BATCH_SIZE:
-        return 
+        return
 
     transitions = memory.sample(BATCH_SIZE)
     batch = Transition(*zip(*transitions))
@@ -132,8 +134,8 @@ def optimize_model(timestep=0, batch_num=0, reward=0):
     criterion = nn.SmoothL1Loss()
     loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
 
-    wandb.log({'loss': loss, 'timestep': timestep, 'batch': batch_num})
-    wandb.log({'loss': loss, 'reward': reward, 'timestep': timestep}) #, 'batch': t})
+    wandb.log({"loss": loss, "timestep": timestep, "batch": batch_num})
+    wandb.log({"loss": loss, "reward": reward, "timestep": timestep})  # , 'batch': t})
 
     # Optimize the model
     optimizer.zero_grad()
@@ -150,15 +152,17 @@ def optimize_model(timestep=0, batch_num=0, reward=0):
 num_episodes = 100
 num_time_per_episode = 500
 
-wandb.config.update({
-    'max_timesteps': num_episodes*num_time_per_episode,
-    'batch_size': BATCH_SIZE,
-    'optimizer': 'Adam',
-    'learning_rate': 'default',
-    'replay_memory': REPLAY_MEMORY_SIZE, # 10000
-    'n_actions': n_actions,
-    'n_observations': n_observations
-})
+wandb.config.update(
+    {
+        "max_timesteps": num_episodes * num_time_per_episode,
+        "batch_size": BATCH_SIZE,
+        "optimizer": "Adam",
+        "learning_rate": "default",
+        "replay_memory": REPLAY_MEMORY_SIZE,  # 10000
+        "n_actions": n_actions,
+        "n_observations": n_observations,
+    }
+)
 
 
 # %%
@@ -172,35 +176,41 @@ for i in range(num_episodes):
     prev_reward = 0
     cur_state = env.reset()
     for j in range(num_time_per_episode):
-        cur_state = torch.tensor ([cur_state], dtype=torch.float32, device=device).unsqueeze(0)
+        cur_state = torch.tensor(
+            [cur_state], dtype=torch.float32, device=device
+        ).unsqueeze(0)
         cur_action = select_action(cur_state)
         # print(cur_action.item())
         next_state, reward = env.step(cur_action.item())
 
         reward = torch.tensor([reward], device=device)
 
-        if j>0:
+        if j > 0:
             memory.push(prev_state, prev_action, prev_reward, cur_state, cur_action)
 
         prev_state = copy.deepcopy(cur_state)
         prev_action = copy.deepcopy(cur_action)
         prev_reward = copy.deepcopy(reward)
 
-        loss = optimize_model(timestep=(i*num_time_per_episode) + j, reward=reward.item())
+        loss = optimize_model(
+            timestep=(i * num_time_per_episode) + j, reward=reward.item()
+        )
         print(f"\rTrain itr: {i}:{j}", end="")
         # print((i*num_time_per_episode) + j, loss, reward.item())
         loss_rec.append(loss)
-        timestep_rec.append((i*num_time_per_episode) + j)
+        timestep_rec.append((i * num_time_per_episode) + j)
         reward_rec.append(reward.item())
 
         target_net_state_dict = target_net.state_dict()
         policy_net_state_dict = policy_net.state_dict()
         for key in policy_net_state_dict:
-            target_net_state_dict[key] = policy_net_state_dict[key]*TAU + target_net_state_dict[key]*(1-TAU)
+            target_net_state_dict[key] = policy_net_state_dict[
+                key
+            ] * TAU + target_net_state_dict[key] * (1 - TAU)
         target_net.load_state_dict(target_net_state_dict)
 
-        if ((i*num_time_per_episode) + j)%100 == 0:
-            SAVE_PATH = './checkpoints/DQN_{}.pt'.format((i*num_time_per_episode) + j)
+        if ((i * num_time_per_episode) + j) % 100 == 0:
+            SAVE_PATH = "./checkpoints/DQN_{}.pt".format((i * num_time_per_episode) + j)
             target_net.load_state_dict(policy_net.state_dict())
             torch.save(policy_net.state_dict(), SAVE_PATH)
 
@@ -240,10 +250,11 @@ def print_policy(policy: torch.nn.Module):
         if s % 25 == 0:
             print()
 
+
 best_reward = -torch.inf
 best_policy = DQN(n_observations, n_actions)
 for i in tqdm(range(100, num_time_per_episode, 100), desc="Evaluating", leave=False):
-    LOAD_PATH = f'./checkpoints/DQN_{i}.pt'
+    LOAD_PATH = f"./checkpoints/DQN_{i}.pt"
     policy_net = DQN(n_observations, n_actions).to(device)
     checkpoint = torch.load(LOAD_PATH)
     policy_net.load_state_dict(checkpoint)
